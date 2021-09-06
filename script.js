a6a779cd4081b3e8880e5858e7c39c1cf357d28e
Netsujr <renatonato_jr@hotmail.com>
2021-09-06 18:50:30 +0900

    changed index script code, still not working

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
-    document.querySelector(".container").classList.toggle("change");
-      });
+  document.querySelector(".container").classList.toggle("change");
+});
