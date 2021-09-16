a89770ccf13cff9b15613ce0bb164bc652762c8c
Netsujr <renatonato_jr@hotmail.com>
2021-09-16 20:22:33 +0900

    done, cards messed up

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 window.onload = () => {
   setTimeout(() => {
-    document.querySelector("body"),classList.add("display");
-  }, 4000);
-}
-
-
-
-
+    document.querySelector("body").classList.add("display");
+  }, 2000);
+};
 
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
