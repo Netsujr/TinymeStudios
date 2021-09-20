7debefda4282331179596465e32770028d5ec49c
Netsujr <renatonato_jr@hotmail.com>
2021-09-20 13:06:16 +0900

    date year fixed

--- script.js
+++ script.js
@@ -14,3 +14,38 @@ setTimeout(() => {
   document.querySelector('html').style.scrollBehavior = 'unset';
 }, 1000);
 });
+
+const date = new Date();
+
+const monthDays = document.querySelector('.days');
+
+const lastDay = new Date(date.getFullYear(), date.getMonth(), 1)
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "Devember"
+];
+
+document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+document.querySelector('.date p').innerHTML = date.toDateString();
+
+let days = "";
+
+for (let i = 1; i <= 31; i++) {
+  days += `<div>${i}</div>`;
+  monthDays.innerHTML = days;
+}
+
+
+
