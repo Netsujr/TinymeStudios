e7d2d2d799d2615c8e0fb115f616583fc22e3b31
Netsujr <renatonato_jr@hotmail.com>
2021-09-20 13:23:10 +0900

    fixed numbers missing

--- script.js
+++ script.js
@@ -19,7 +19,13 @@ const date = new Date();
 
 const monthDays = document.querySelector('.days');
 
-const lastDay = new Date(date.getFullYear(), date.getMonth(), 1)
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+const firstDayIndex = date.getDay();
+
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
 const months = [
   "January",
@@ -42,7 +48,11 @@ document.querySelector('.date p').innerHTML = date.toDateString();
 
 let days = "";
 
-for (let i = 1; i <= 31; i++) {
+for (let x = firstDayIndex; x > 0; x--) {
+  days += `<div class="prev-date">${prevLastDay - x + 1}</div>`
+}
+
+for (let i = 1; i <= lastDay; i++) {
   days += `<div>${i}</div>`;
   monthDays.innerHTML = days;
 }
